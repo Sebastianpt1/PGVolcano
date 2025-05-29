31992d816a1689503d55e734807943e4dc1f8b1d
juan montiel <jjmontiel19@gmail.com>
2025-05-29 15:59:11 -0600

    Añadi camera.h y cpp del movimiento del personaje(aun es una camara y no tenemos cubo(modelado)) todo esta desconectado todo con el main principal afuera

---
+++ Personaje/Camera.h
@@ -0,0 +1,41 @@
+#pragma once
+#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+class Camera {
+public:
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+
+    float Yaw;
+    float Pitch;
+
+    float MovementSpeed;
+    float MouseSensitivity;
+
+    Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch);
+
+    glm::mat4 GetViewMatrix();
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    void ProcessMouseMovement(float xoffset, float yoffset);
+
+private:
+    void updateCameraVectors();
+};
+
+#endif
+#pragma once
