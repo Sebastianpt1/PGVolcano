723aed09f14ccf44b46129ec9574af8ca10b1147
juan montiel <jjmontiel19@gmail.com>
2025-05-29 15:55:47 -0600

    Añadi parte del personaje su funcion y fisica(por separdo, esto se volvera una clase para el main principal)

--- Personaje/Personaje.cpp
+++ Personaje/Personaje.cpp
@@ -1,6 +1,348 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-void saludar() {
-    std::cout << "Hola desde Personaje!" << std::endl;
 
-    //Hollaaaa
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h" 
+#include <fstream>
+#include <sstream>
+#include "Camera.h"
+
+Camera camera(glm::vec3(0.0f, 1.0f, 3.0f),
+    glm::vec3(0.0f, 1.0f, 0.0f),
+    -90.0f, 0.0f);
+
+//fps
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+//fisicas 
+float velocityY = 0.0f;
+bool isJumping = false;
+bool isOnGround = true;
+
+
+//variables globales de mause para girar camara
+float lastX = 400;
+float lastY = 300;
+bool firstMause = true;
+
+
+
+
+float vertices[] = {
+    // posiciones         // coordenadas de textura
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+float floorVertices[] = {
+    // posiciones           // tex coords
+    -5.0f, 0.0f, -5.0f,     0.0f, 0.0f,
+     5.0f, 0.0f, -5.0f,     1.0f, 0.0f,
+     5.0f, 0.0f,  5.0f,     1.0f, 1.0f,
+
+     5.0f, 0.0f,  5.0f,     1.0f, 1.0f,
+    -5.0f, 0.0f,  5.0f,     0.0f, 1.0f,
+    -5.0f, 0.0f, -5.0f,     0.0f, 0.0f
+};
+bool isOverFloor(const glm::vec3& pos) {
+    return (pos.x >= -5.0f && pos.x <= 5.0f &&
+        pos.z >= -5.0f && pos.z <= 5.0f);
 }
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    float speed = camera.MovementSpeed;
+
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        speed *= 2.0f;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime * speed);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime * speed);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime * speed);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime * speed);
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && isOnGround) {
+        velocityY = 5.0f;
+        isJumping = true;
+        isOnGround = false;
+
+    }
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMause) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMause = false;
+    }
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+
+
+}
+
+
+
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hola Triangulo", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "No se pudo crear la ventana GLFW\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Fallo al inicializar GLAD\n";
+        return -1;
+    }
+
+    Shader shader("Default.vert", "Default.frag");
+
+
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+
+
+
+
+
+    //archivos VAO, VBO
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //VERTICES CUBO 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    // vao y vbo del piso
+    unsigned int floorVAO, floorVBO;
+    glGenVertexArrays(1, &floorVAO);
+    glGenBuffers(1, &floorVBO);
+
+    glBindVertexArray(floorVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, floorVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(floorVertices), floorVertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    //Configuracion de envoltura y filtrado
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+    //CARGAR IMAGEN
+    int width, height, nrchannels;
+    unsigned char* data = stbi_load("Texture/pasto.png", &width, &height, &nrchannels, 0);
+    if (data) {
+        GLenum format = GL_RGB;
+        if (nrchannels == 4) format = GL_RGBA;
+
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    else {
+        std::cout << "fallo al cargar textura";
+
+    }
+    stbi_image_free(data);
+
+
+
+    //bucle principal
+    while (!glfwWindowShouldClose(window)) {
+
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+
+        processInput(window);
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        shader.use();
+        shader.setInt("texture1", 0); //utiliza la unidad 0
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+
+        shader.setMat4("model", glm::value_ptr(model));
+        shader.setMat4("view", glm::value_ptr(view));
+        shader.setMat4("projection", glm::value_ptr(projection));
+
+
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+
+        // Piso
+        glm::mat4 floorModel = glm::mat4(1.0f);
+        shader.setMat4("model", glm::value_ptr(floorModel));
+        glBindVertexArray(floorVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+
+        //gravedad y salto
+        if (isOverFloor(camera.Position)) {
+            // Si estamos cayendo
+            if (!isOnGround) {
+                velocityY -= 9.8f * deltaTime;
+                camera.Position.y += velocityY * deltaTime;
+
+                // ¿Tocamos el piso?
+                if (camera.Position.y <= 1.0f) {
+                    camera.Position.y = 1.0f;
+                    velocityY = 0.0f;
+                    isJumping = false;
+                    isOnGround = true;
+                }
+            }
+
+            // Si no estamos saltando ni cayendo, mantener altura fija
+            if (camera.Position.y < 1.0f) {
+                camera.Position.y = 1.0f;
+                velocityY = 0.0f;
+                isJumping = false;
+                isOnGround = true;
+            }
+        }
+        else {
+            // Si no está sobre el piso, aplicar gravedad
+            velocityY -= 9.8f * deltaTime;
+            camera.Position.y += velocityY * deltaTime;
+        }
+
+        //limitacion de altura
+        if (!isOverFloor(camera.Position) && camera.Position.y <= -10.0f) {
+            camera.Position = glm::vec3(0.0f, 5.0f, 0.0f); // reaparece
+            velocityY = 0.0f;
+        }
+
+
+
+
+
+
+
+
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+
+
+
+    }
+
+    glfwTerminate();
+    return 0;
+
+
+
+
+
+
+
+
+}
+
